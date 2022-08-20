0a9ab0e0092a847f5b1cd772975b5c6eb09d0cee
Mauricio Cerqueira <106408932+mauricio-cerqueira@users.noreply.github.com>
2022-08-20 17:06:38 -0300

    Adicionar arquivos de projeto.

---
+++ HTML_CSS_JS/JavaScript/JavaScript.js
@@ -0,0 +1,20 @@
+﻿const dt = new Date().toLocaleString();
+
+document.getElementById("data").innerHTML = dt;
+
+
+function FuncaoAviso() {
+    alert('Este é apenas um painel de avisos!');
+}
+
+let tabuleiro = [0,0,0,0,0,0,0,0,0,0];
+
+function jogar(bot) {
+    if (tabuleiro[bot] == "0") {
+        document.getElementById(bot).innerHTML = "X";
+        tabuleiro[bot] = "X";
+    } else {
+        alert("Escolha outra posição!");
+    }
+    
+}
---
+++ HTML_CSS_JS/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As Informações Gerais sobre um assembly são controladas por meio do 
+' conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Verificar os valores dos atributos do conjunto
+<Assembly: AssemblyTitle("HTML_CSS_JS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HTML_CSS_JS")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+<Assembly: Guid("84807496-25b8-46aa-95d1-5d5bf973d26d")>
+
+' As informações de versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' É possível especificar todos os valores ou utilizar como padrão os Números da Versão e de Revisão 
+' usando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
